4088a1b2170866fb08ce4c312935e88ca6ae7560
thomasdvorsky <135068311+thomasdvorsky@users.noreply.github.com>
2025-03-09 18:29:46 -0400

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -9,3 +9,12 @@ secret_number = random.randint(1,100)
 
 # Debugging line (remove later) - Shows the secret number for testing
 print(f"(Debug: The secret number is {secret_number})")
+
+# Ask the player for a guess
+guess = int(input("Enter your guess (1-100_:"))
+
+# Check if the guess is correct
+if guess == secret_number:
+    print("Congrats! You guessed the correct number!")
+  Else:
+    print("Wrong guess! Try again next time.")
