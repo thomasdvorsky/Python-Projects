8c43f3f815caddab15a1c3c3e8c6504b79a1443e
thomasdvorsky <135068311+thomasdvorsky@users.noreply.github.com>
2025-03-09 17:55:47 -0400

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,3 +1,11 @@
 print("🎯 Welcome to the Number Guessing Game!")
 print("I have picked a random number between 1 and 100.")
 print("Can you guess what it is?")
+
+import random # Import the random module
+
+# Generate a random number between 1 and 100
+secret_number = random.randint(1,100)
+
+# Debugging line (remove later) - Shows the secret number for testing
+print(f"(Debug: The secret number is {secret_number})")
