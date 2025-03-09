69e2b2a47b40de7e3bc2545b1d40bf905cb4705d
thomasdvorsky <135068311+thomasdvorsky@users.noreply.github.com>
2025-03-09 18:56:38 -0400

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,14 +1,14 @@
+import random # Import the random module
+
 print("🎯 Welcome to the Number Guessing Game!")
 print("I have picked a random number between 1 and 100.")
 print("Can you guess what it is?")
-
-import random # Import the random module
 
 # Generate a random number between 1 and 100
 secret_number = random.randint(1,100)
 
 # Ask the player for a guess
-guess = int(input("Enter your guess (1-100_:"))
+guess = int(input("Enter your guess (1-100:"))
 
 # Check if the guess is correct
 if guess == secret_number:
